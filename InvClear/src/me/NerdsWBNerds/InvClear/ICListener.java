63e622c1ee1fe695492e84925dabd83064bb4daf
NerdsWBNerds <richcoll97@gmail.com>
2012-07-22 04:04:13 -0500

    Initial commit

---
+++ InvClear/src/me/NerdsWBNerds/InvClear/ICListener.java
@@ -0,0 +1,11 @@
+package me.NerdsWBNerds.InvClear;
+
+import org.bukkit.event.Listener;
+
+public class ICListener implements Listener {
+	public InvClear plugin;
+	
+	public ICListener(InvClear pt) {
+		plugin = pt;
+	}
+}
